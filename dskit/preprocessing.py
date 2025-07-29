af6371e8881ba676d290fdbf6a97f8273d2ba402
Iwan <grabageheap@inbox.lv>
2025-07-29 11:27:07 +0300

    preprocessing.py submodule added

---
+++ dskit/preprocessing.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+
+def merge_rare(series: pd.Series, threshold: float = 0.01, default: str = 'OTHER') -> pd.Series:
+    abs_f = series.value_counts(dropna=False) / len(series)
+
+    top_values = abs_f[abs_f >= threshold].index.tolist()
+
+    return pd.Series(index=series.index, data=np.where(series.isin(top_values), series, default))
