bec0f3c18fba792c77ac61b4e2c5c7009da2b809
Iwan <grabageheap@inbox.lv>
2025-07-29 11:51:58 +0300

    qcut_enumerate_float method added to preprocessing.py

--- dskit/preprocessing.py
+++ dskit/preprocessing.py
@@ -8,3 +8,18 @@ def merge_rare_values(series: pd.Series, threshold: float = 0.01, default: str =
     top_values = abs_f[abs_f >= threshold].index.tolist()
 
     return pd.Series(index=series.index, data=np.where(series.isin(top_values), series, default))
+
+
+def qcut_enumerate_float(x: pd.Series, q=10) -> pd.Series:
+    t = pd.qcut(x, q=q)
+
+    bins = t.value_counts(dropna=False).index.sort_values().tolist()
+
+    d = {b: i for i, b in enumerate(bins, start=1)}
+
+    t = t.map(d)
+
+    if np.nan in d:
+        return t.cat.add_categories(0).fillna(0)
+    else:
+        return t
