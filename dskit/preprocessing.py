a13c0cd19be7c6bf90e02953da504bfe25286fac
Iwan <grabageheap@inbox.lv>
2025-07-29 11:59:42 +0300

    process_zeros flag added to enumerate_qcut method

--- dskit/preprocessing.py
+++ dskit/preprocessing.py
@@ -10,14 +10,23 @@ def merge_rare_values(series: pd.Series, threshold: float = 0.01, default: str =
     return pd.Series(index=series.index, data=np.where(series.isin(top_values), series, default))
 
 
-def enumerate_qcut(x: pd.Series, q=10) -> pd.Series:
+def enumerate_qcut(x: pd.Series, q=10, process_zeros: bool = False) -> pd.Series:
+    if process_zeros:
+        x_0 = x[x == 0]
+        x = x[x != 0]
+    else:
+        x_0 = pd.Series()
+
     t = pd.qcut(x, q=q)
 
     bins = t.value_counts(dropna=False).index.sort_values().tolist()
 
-    d = {b: i for i, b in enumerate(bins, start=1)}
+    d = {b: b.left for i, b in enumerate(bins, start=1)}
 
     t = t.map(d)
+
+    if process_zeros:
+        t = pd.concat(objs=[x_0, t])
 
     if np.nan in d:
         return t.cat.add_categories(0).fillna(0)
