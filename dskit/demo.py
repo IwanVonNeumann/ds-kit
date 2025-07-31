be28e6ea3cc429c2dbadc9d391e8629d1487772d
Iwan <grabageheap@inbox.lv>
2025-07-31 10:41:02 +0300

    'children_count' added to demo df

--- dskit/demo.py
+++ dskit/demo.py
@@ -25,4 +25,6 @@ def generate_demo_df(size: int) -> pd.DataFrame():
     df.loc[index_b, 'risk'] = np.random.choice([0, 1], p=[0.90, 0.10], size=n_b)
     df.loc[index_c, 'risk'] = np.random.choice([0, 1], p=[0.80, 0.20], size=n_c)
 
+    df['children_count'] = np.random.choice([np.nan, 0, 1, 2, 3, 4], p=[0.05, 0.3, 0.35, 0.2, 0.08, 0.02], size=size)
+
     return df
